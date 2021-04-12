5b87734a157a121b787c54f842c26784098d4b5e
Alejandro Martinez <alejomartinez8@gmail.com>
2021-04-11 22:31:59 -0500

    Test heroku deploy

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-// swagger docs route
-app.use('/api-docs', require('./helpers/swagger'));
-
 // api routes
+app.use('/', (req, res) => {
+  res.send('Delilah-resto-API');
+});
+
 app.use('/users', usersController);
 app.use('/products', productsController);
+
+// swagger docs route
+app.use('/api-docs', require('./helpers/swagger'));
 
 // global error handler
 app.use(errorHandler);
