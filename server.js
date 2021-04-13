865de001266303e974c7c46e16af977ddab87c1e
Alejandro Martinez <alejomartinez8@gmail.com>
2021-04-12 22:48:56 -0500

    Update port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api-docs', require('./helpers/swagger'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
